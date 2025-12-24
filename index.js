16f4564b79642b87920a30d8ca9e2a0617647ea9
HQ-Binh <binh.hq@liena.v>
2025-12-24 08:51:57 +0000

    nuxt layer

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as LaBtn } from './app/components/la/btn.vue';
+export { default as LaBtn } from './app/components/la/btntest.vue';
 export { default as LaChip } from './app/components/la/chip.vue';
