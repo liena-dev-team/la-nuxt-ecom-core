bb550bd6b114bd6558922d6caef9762cd686f5f4
HQ-Binh <binh.hq@liena.v>
2025-12-25 07:19:15 +0000

    change status

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as LaBtn } from './app/components/la/btntest.vue';
+export { default as LaBtnTest } from './app/components/la/btntest.vue';
 export { default as LaChip } from './app/components/la/chip.vue';
